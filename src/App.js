07fa3ee94e215e32ab0456c3e6f1a597215b956a
Mark Gowen <mark.gowen78@gmail.com>
2019-09-30 14:30:25 -0700

    project compeleted
    completed project.
    adding styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,140 @@
 import React from 'react';
 
+// Components
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+// Material UI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import CheckIcon from '@material-ui/icons/Check';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200
+      }
+    }
+  }
+}));
+
+const classes = useStyles();
+
+const todoData = [
+  {
+    task: '',
+    id: Date.now(),
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: todoData
+    };
+  }
+
+  toggleTodo = id => {
+    console.log(id);
+    this.setState({
+      tasks: this.state.tasks.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  addTask = taskName => {
+    const newTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    };
+    console.log(newTask);
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    });
+  };
+
+  clearTask = () => {
+    this.setState({
+      tasks: this.state.tasks.filter(item => !item.completed)
+    });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Typography className={classes.title} variant="h6" noWrap>
+            Todo App
+          </Typography>
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTask={this.addTask} />
+        </AppBar>
+        <TodoList
+          tasks={this.state.tasks}
+          toggleTodo={this.toggleTodo}
+          clearTask={this.clearTask}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+import Todo from './Todo'
+
+const TodoList = props => {
+    console.log(props.tasks)
+    return (
+        <div className='task-list'>
+            {props.tasks.map(item => (
+                <Todo key={item.id} item={item} toggleTodo={props.toggleTodo} />
+            ))}
+            <button className='clear-btn' onClick={props.clearTask}>
+                Clear Task
+            </button>
+        </div>
+    );
+};
+
+export default TodoList;
+
